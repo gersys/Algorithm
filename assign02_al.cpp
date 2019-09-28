56ad66f10e3d3a7a13582a372290735cb98089d7
Cheol-Ho Cho <hoonchcho@gmail.com>
2019-09-28 14:49:15 +0900

    assign02 : answer code

--- assign02_al.cpp
+++ assign02_al.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 #define MAX 1000;
 
+int recurrsion(int start, int end, int count, int data[1000], int K) {
+	int sum=data[start] +data[end];
+	if (start >= end)
+		return count;
+	else {
+		if (sum > K) {
+			end--;
+			recurrsion(start, end, count, data, K);
+		}
+		else if (sum < K) {
+			start++;
+			recurrsion(start, end, count, data, K);
+		}
+		else {
+			count++;
+			start++;
+			end--;
+			recurrsion(start, end, count, data, K);
+		}
+	}
+
+}
+
 int main() {
 	int data[1000];
 	int N, K;
@@ -11,26 +34,38 @@ int main() {
 
 	cin >> N;
 
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++) {
 		cin >> data[i];
+		cout << data[i] << " ";
+	}
+	cout << endl;
+	
+	cin >> K;
 
 	int start = 0, end = N - 1;
 	int count = 0;
 
-	while (start < end) {
-		int sum = data[start] + data[end];
-		if (sum > K)
-			end--;
-		else if (sum < K)
-			start++;
-		else {
-			count++;
-			start++;
-			end--;
-		}
-	}
+	count = recurrsion(start, end, count, data, K);
+
 
 	cout << "count:" << count << endl;
+
+	//
+//
+//while (start < end) {
+//	int sum = data[start] + data[end];
+//	if (sum > k)
+//		end--;
+//	else if (sum < k)
+//		start++;
+//	else {
+//		count++;
+//		start++;
+//		end--;
+//	}
+//}
+
+
 
 
 }
