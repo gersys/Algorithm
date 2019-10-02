5c9a7cf6cbeb2ef017a613658eb9428052370ae7
Cheol-Ho Cho <hoonchcho@gmail.com>
2019-10-02 22:33:44 +0900

    assingn03_1_al: add 'search module'

--- assign03_1_al.cpp
+++ assign03_1_al.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<stack>
 
 using namespace std;
 
-#define Searched 2;
+#define Searched 2
+#define NORTH 3
+#define EAST 4
+#define SOUTH 5
+#define WEST 6
 
 void show_maze(int maze[10][10], int N);
 int search_path(int maze[10][10], int N,int K);
-void next_pos(int maze[10][10],int N);
+bool next_pos(int maze[10][10],int N);
+void put_back(int prev);
 
 int x = 0; //definite starting point
 int y = 0;
+int num = 0;
+
+stack<int> s;
 
 int main() {
 	ifstream file("input1.txt");
@@ -43,13 +52,20 @@ int main() {
 
 int search_path(int maze[10][10], int N,int K) {
 	maze[y][x] = Searched; // Mark current position as 'Searched'
-	next_pos(maze, N);
+	bool possible=next_pos(maze, N);
 	cout << "x:" << x << "y:" << y << endl;
+	cout << "num:" << num << endl;
 	show_maze(maze, N);
 	cout << "------------------------" << endl;
 
-	if (x == -1 && y == -1)
-		return -1;
+	if (possible == false) {
+		cout << "there is a no way to go" << endl;
+		int prev = s.top();
+		s.pop();
+		put_back(prev);
+	}
+	num++; // num of move ++
+		
 
 	int num = search_path(maze, N, K);
 
@@ -57,26 +73,50 @@ int search_path(int maze[10][10], int N,int K) {
 	return 1;
 }
 
-void next_pos(int maze[10][10],int N) {
+void put_back(int prev) {
+	if (prev == NORTH) {
+		num--;
+		y++;
+	}
+	else if (prev == EAST) {
+		num--;
+		x--;
+	}
+	else if (prev == SOUTH) {
+		num--;
+		y--;
+	}
+	else if (prev == WEST) {
+		num--;
+		x++;
+	}
+
+}
+
+bool next_pos(int maze[10][10],int N) {
 	// Sequace: N,E,S,W
 	if (y > 0 && maze[y - 1][x] == 0) { //N
 		y--;
-		return;
+		s.push(NORTH);
+		return true;
 	}
 	else if (x < N && maze[y][x + 1] == 0) {//E
 		x++;
-		return;
+		s.push(EAST);
+		return true;
 	}
 	else if (y < N && maze[y + 1][x] == 0) { //S
 		y++;
-		return;
+		s.push(SOUTH);
+		return true;
 	}
 	else if (x < 0 && maze[y][x - 1] == 0) {//W
 		x--;
-		return;
+		s.push(WEST);
+		return true;
 	}
 	else
-		return;
+		return false;
 }
 
 void show_maze(int maze[10][10], int N) {
