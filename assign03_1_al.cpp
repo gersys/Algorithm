65284ee870475e70bac885f37e091865d2e12155
Cheol-Ho Cho <hoonchcho@gmail.com>
2019-10-03 16:40:22 +0900

    assingn03_1_al: solution

--- assign03_1_al.cpp
+++ assign03_1_al.cpp
@@ -1,36 +1,35 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-#include<stack>
 
 using namespace std;
 
-#define Searched 2
-#define NORTH 3
-#define EAST 4
-#define SOUTH 5
-#define WEST 6
+#define MAX 16
+#define PATH 0
+#define WALL 1
+#define VISITED 2
+#define BLOCKED 3
+
+
+int num = 0;
+int sol=0;
 
 void show_maze(int maze[10][10], int N);
-int search_path(int maze[10][10], int N,int K);
-bool next_pos(int maze[10][10],int N);
-void put_back(int prev);
+int findMazePath(int x, int y, int N, int K, int maze[10][10], int dist);
 
-int x = 0; //definite starting point
-int y = 0;
-int num = 0;
-
-stack<int> s;
 
 int main() {
+
 	ifstream file("input1.txt");
 	string input;
+
 
 	file >> input; // T
 
 	int T = stoi(input);
+	int x = 0;
+	int y = 0;
 
-	cout << "T:" << T << endl;
 
 	for (int i = 0; i < T; i++) {
 		file >> input;
@@ -42,81 +41,42 @@ int main() {
 				maze[i][j] = stoi(input);
 			}
 		show_maze(maze, N); // Check if the maze generation was done well 
+		cout << "---------------------------" << endl;
 
 		file >> input;
 		int K = stoi(input); // K
-		int num=search_path(maze, N,K);
-		cout << num << endl;
+		sol=findMazePath(0, 0, N, K, maze,0);
+		show_maze(maze, N);
+		cout << "---------------------------" << endl;
+		cout << "sol:" <<sol << endl;
 	}
+
+	return 0;
 }
 
-int search_path(int maze[10][10], int N,int K) {
-	maze[y][x] = Searched; // Mark current position as 'Searched'
-	bool possible=next_pos(maze, N);
-	cout << "x:" << x << "y:" << y << endl;
-	cout << "num:" << num << endl;
-	show_maze(maze, N);
-	cout << "------------------------" << endl;
-
-	if (possible == false) {
-		cout << "there is a no way to go" << endl;
-		int prev = s.top();
-		s.pop();
-		put_back(prev);
+int findMazePath(int x, int y,int N,int K, int maze[10][10],int dist) {
+	int sum = 0;
+	if (x < 0 || y < 0 || x >= N || y >= N)
+		return 0;
+	else if (dist > K)
+		return 0;
+	else if (maze[x][y] != PATH)
+		return 0;
+	else if (x == N - 1 && y == N - 1) {
+		maze[x][y] = PATH;
+		return 1;
 	}
-	num++; // num of move ++
-		
-
-	int num = search_path(maze, N, K);
-
-	
-	return 1;
-}
-
-void put_back(int prev) {
-	if (prev == NORTH) {
-		num--;
-		y++;
-	}
-	else if (prev == EAST) {
-		num--;
-		x--;
-	}
-	else if (prev == SOUTH) {
-		num--;
-		y--;
-	}
-	else if (prev == WEST) {
-		num--;
-		x++;
+	else {
+		maze[x][y] = VISITED;
+		sum += findMazePath(x - 1, y, N,K, maze, dist + 1) + findMazePath(x, y - 1, N, K, maze, dist + 1) + findMazePath(x + 1, y, N, K, maze, dist + 1) + findMazePath(x, y + 1, N, K, maze, dist + 1);
+		if (sum > 0) {
+			maze[x][y] = PATH;
+			return sum;
+		}
+		maze[x][y] = PATH;
+		return 0;
 	}
 
-}
-
-bool next_pos(int maze[10][10],int N) {
-	// Sequace: N,E,S,W
-	if (y > 0 && maze[y - 1][x] == 0) { //N
-		y--;
-		s.push(NORTH);
-		return true;
-	}
-	else if (x < N && maze[y][x + 1] == 0) {//E
-		x++;
-		s.push(EAST);
-		return true;
-	}
-	else if (y < N && maze[y + 1][x] == 0) { //S
-		y++;
-		s.push(SOUTH);
-		return true;
-	}
-	else if (x < 0 && maze[y][x - 1] == 0) {//W
-		x--;
-		s.push(WEST);
-		return true;
-	}
-	else
-		return false;
 }
 
 void show_maze(int maze[10][10], int N) {
@@ -125,5 +85,5 @@ void show_maze(int maze[10][10], int N) {
 			cout << maze[i][j];
 		cout << endl;
 	}
-			
+
 }
