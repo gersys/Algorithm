f424dc585b79b9417381b45436e9468a1f24f383
Cheol-Ho Cho <hoonchcho@gmail.com>
2019-10-10 22:25:18 +0900

    assign04_1 : add quick

--- assign04_1_al.cpp
+++ assign04_1_al.cpp
@@ -13,6 +13,8 @@ void selection(int* arr, int N);
 void insertion(int* arr, int N);
 void mergeSort(int* arr, int s, int N);
 void merge(int* arr, int s, int m, int e);
+void quickSort(int* arr, int s, int e);
+int partition(int* arr, int s, int e);
 
 int main() {
 
@@ -37,10 +39,10 @@ int main() {
 	for (int i = 0; i < N1; i++) 
 		arr1[i] = rand() % N1;
 	
-	for (int i = 0; i < N1; i++)
+	for (int i = 0; i < N2; i++)
 		arr2[i] = rand() % N2;
 	
-	for (int i = 0; i < N1; i++)
+	for (int i = 0; i < N3; i++)
 		arr3[i] = rand() % N3;
 
 	// examine if array1 is well created
@@ -53,7 +55,8 @@ int main() {
 	//bubble(arr1, N1);
 	//selection(arr1, N1);
 	//insertion(arr1, N1);
-	mergeSort(arr1, 0, N1-1);
+	//mergeSort(arr1, 0, N1-1);
+	quickSort(arr1, 0, N1 - 1);
 
 	//examine arr1 is well sorted
 
@@ -150,4 +153,33 @@ void merge(int* arr, int s, int m, int e) {
 
 	for (int i = s; i <= e; i++)
 		arr[i] = tmp[i];
+}
+
+void quickSort(int* arr, int s, int e) {
+
+	if (s < e) {
+		int m=partition(arr, s, e);
+		quickSort(arr, s, m-1);
+		quickSort(arr, m + 1, e);
+		
+
+	}
+
+}
+
+int partition(int* arr, int s , int e) {
+	int pivot = arr[e];
+	int i = s - 1;
+	for (int j = s; j < e; j++) 
+		if (arr[j] < pivot) {
+			i = i + 1;
+			int temp = arr[j];
+			arr[j] = arr[i];
+			arr[i] = temp;
+		}
+	int temp = pivot;
+	arr[e] = arr[i + 1];
+	arr[i + 1] = temp;
+
+	return i + 1;
 }
